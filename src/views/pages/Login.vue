a8ca1e43fbdf818f81b542be207a74957d9cd498
khanhjm <khanhjk@gmail.com>
2022-02-23 23:43:40 +0700

    chọn ô và kiểm tra tổng 15

--- src/views/pages/Login.vue
+++ src/views/pages/Login.vue
@@ -1,10 +1,100 @@
 <template>
-  Login
+  <div id="game">
+    <h1>Game</h1>
+    <section class="game-board">
+      <Card
+        v-for="(card, index) in cardList"
+        :key="`card-${index}`"
+        :value="card.value"
+        :matched="card.matched"
+        :visible="card.visible"
+        :position="card.position"
+        @select-card="flitCard"
+      />
+    </section>
+    <h2>{{ status }}</h2>
+  </div>
 </template>
 
 <script>
-export default {}
+import { ref, watch } from "vue";
+import Card from "../../components/Card";
+export default {
+  name: "App",
+  components: {
+    Card,
+  },
+  setup() {
+    const cardList = ref([]);
+    const userSelection = ref([]);
+    const status = ref("");
+    for (let i = 0; i < 16; i++) {
+      cardList.value.push({
+        value: i,
+        visible: false,
+        position: i,
+        matched: false,
+      });
+    }
+
+    const flitCard = (payload) => {
+      cardList.value[payload.position].visible = !cardList.value[payload.position]
+        .visible;
+
+      if (userSelection.value[0]) {
+        userSelection.value[1] = payload;
+      } else {
+        userSelection.value[0] = payload;
+      }
+    };
+
+    watch(
+      userSelection,
+      (currentValue) => {
+        if (currentValue.length === 2) {
+          const cardOne = currentValue[0];
+          const cardTwo = currentValue[1];
+
+          if (cardOne.faceValue + cardTwo.faceValue == 15) {
+            status.value = "Matched";
+            cardList.value[cardOne.position].matched = true;
+            cardList.value[cardTwo.position].matched = true;
+          } else {
+            cardList.value[cardOne.position].visible = false;
+            cardList.value[cardTwo.position].visible = false;
+          }
+
+          userSelection.value.length = 0;
+        }
+      },
+      { deep: true }
+    );
+
+    return {
+      cardList,
+      flitCard,
+      userSelection,
+      status,
+    };
+  },
+};
 </script>
 
 <style>
+#game {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 20px;
+}
+
+.game-board {
+  display: grid;
+  grid-template-columns: 100px 100px 100px 100px;
+  grid-template-rows: 100px 100px 100px 100px;
+  grid-column-gap: 30px;
+  grid-row-gap: 30px;
+  justify-content: content;
+}
 </style>
